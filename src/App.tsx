a343b85f39d6a6a960a5fcc4c15f4c3545166e7e
Antonio Junior <antoniosaraiva.gois@gmail.com>
2022-11-22 21:51:32 -0300

    feat: add splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
+import { lazy, Suspense } from "react";
+import { Splash } from "./pages/Splash";
 import { Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="login" element={<Login/>}/>
-    </Routes>
+    <Suspense fallback={<Splash/>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login/>}/>
+      </Routes>
+    </Suspense>
   )
 };
 
