5cad79c151b98e106d39acce8f80f18fec49c252
NicholasTavares <nicholasbalby@hotmail.com>
2023-01-18 21:49:03 -0300

    refactor: index named imports assets

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export {default as LogoDark} from './buscapets-logo-dark.png';
+export {default as GoogleIcon} from './google-icon.png';
+export {default as DogWalkingIcon} from './undraw_dog_walking.png';
+export {default as ForgotPasswordVector} from './vector-forgot-password.svg';
+export {default as DogNotFoundIcon} from './dog-not-found.svg';
