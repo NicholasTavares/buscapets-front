7520201bd9429b924d89acd36848f4e1d7863ef9
NicholasTavares <nicholasbalby@hotmail.com>
2023-02-07 21:45:57 -0300

    refactor: test automated att CI/CD with logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
-export {default as LogoDark} from './buscapets-logo-dark.png';
-export {default as GoogleIcon} from './google-icon.png';
-export {default as DogWalkingIcon} from './undraw_dog_walking.png';
-export {default as ForgotPasswordVector} from './vector-forgot-password.svg';
-export {default as DogNotFoundIcon} from './dog-not-found.svg';
+export { default as LogoDark } from './buscapets-logo-dark.png';
+export { default as LogoWhite } from './buscapets-logo.png';
+export { default as GoogleIcon } from './google-icon.png';
+export { default as DogWalkingIcon } from './undraw_dog_walking.png';
+export { default as ForgotPasswordVector } from './vector-forgot-password.svg';
+export { default as DogNotFoundIcon } from './dog-not-found.svg';
